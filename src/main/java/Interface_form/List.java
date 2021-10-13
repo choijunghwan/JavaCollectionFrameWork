e4986eb01d1749ea895890b2939350694a38fb87
ChoiJungHwan <vkdlxj3562@naver.com>
2021-10-13 16:40:02 +0900

    Initial commit

---
+++ src/main/java/Interface_form/List.java
@@ -0,0 +1,10 @@
+package Interface_form;
+
+/**
+ * 자바 List Interface입니다.
+ * List는 ArrayList, SinglyLinkedList, DoublyLinked에 의해 각각 구현됩니다.
+ *
+ * @param <E> the type of elements in this list
+ */
+public interface List<E> {
+}
